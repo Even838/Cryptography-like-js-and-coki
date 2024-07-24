8051f32f7adf1b910dfe74b20ce1be13a84cd6c8
Even-dev <samuca1234.oficial@gmail.com>
2024-07-24 11:45:23 -0300

    Atualizar o Main.js

--- Main.js
+++ Main.js
@@ -3,3 +3,9 @@
       const criptografado = btoa(input);
       document.getElementById('saida').textContent = criptografado;
     }
+
+    function desc() {
+      const inp = document.getElementById('des').value;
+      const textoOriginal = atob(inp);
+      document.getElementById('resultado').textContent = textoOriginal;
+    }
